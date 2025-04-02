f53d1ff314525f6132798b49c0292539206c05ce
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2025-04-02 17:30:29 +0900

    feat: 20250402 심화 과제 제출

---
+++ 20250402/심화/axios-test-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>콘솔을 확인합니다.</h2>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+const requestAPI = () => {
+  axios
+    .get('/api/todos')
+    .then((response) => {
+      console.log('# 전체 Todo 목록:', response.data);
+
+      return axios.get('/api/todos/1');
+    })
+    .then((response) => {
+      console.log('# ID 1 Todo:', response.data);
+    })
+    .catch((error) => {
+      console.error('에러 발생:', error);
+    });
+};
+
+requestAPI();
+</script>
---
+++ 20250402/심화/axios-test-app/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+// import App from './App.vue'
+// import App from './App2.vue';
+// import App from './App4.vue';
+import App from './App6.vue';
+
+createApp(App).mount('#app');
